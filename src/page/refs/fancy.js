c5f0c2e8a1fd0f810a293b3624f5f9279883b9a0
wujing <j.u.sophia@qq.com>
2018-11-30 11:51:14 +0800

    refs

---
+++ src/page/refs/fancy.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class fancy extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div>
+            <h3>这将有一个button</h3>
+
+        </div>)
+    }
+}
+
+export default fancy;
