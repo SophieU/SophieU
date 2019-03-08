e2edc8d9abd39d571cf63d94f2e4fdbf9248dd86
SophieU <j.u.sophia@qq.com>
2019-03-08 11:57:10 +0800

    total

--- 文档记录/test.js
+++ 文档记录/test.js
@@ -1,1 +1,16 @@
-console.log(2**4)
+function sleep(){
+    return new Promise(resolve=>{
+        setTimeout(function () {
+            console.log('finish');
+            resolve("sleep");
+            console.log(111)
+        }, 2000);
+    })
+}
+
+async function test(){
+    let value = await sleep();
+    console.log(value)
+    console.log("object");
+}
+test();
