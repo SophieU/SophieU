2c2b18ba795032ce9af6a451539539a4444c72f0
SophieU <j.u.sophia@qq.com>
2019-05-28 15:30:32 +0800

    wow3

--- hello.ts
+++ hello.ts
@@ -2,5 +2,14 @@ class Animal{
     constructor(name) {
         this.name = name;
     }
-    
+    get name() {
+        return 'Jack';
+    }
+    set name(value) {
+        console.log('setter '+value)
+    }
 }
+
+let a = new Animal('Kitty');
+a.name = 'Tom';
+console.log(a.name)
