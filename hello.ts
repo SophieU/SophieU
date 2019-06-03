cc70f85a10233ec204ec7b6786cd85efe760b8ee
SophieU <j.u.sophia@qq.com>
2019-06-03 15:29:23 +0800

    wow3

--- hello.ts
+++ hello.ts
@@ -2,19 +2,5 @@ class Animal{
     constructor(name) {
         this.name = name;
     }
-    sayHi() {
-        return `My name is ${this.name}`
-    }
+    
 }
-
-class Cat extends Animal{
-    constructor(name) {
-        super(name);
-        console.log(this.name)
-    }
-    sayHi() {
-        return `Meow,${super.sayHi()}`
-    }
-}
-let c = new Cat('tom')
-console.log(c.sayHi())
