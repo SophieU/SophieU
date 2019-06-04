d4078561931ea66b198b830d79227f2cc97bd5d4
SophieU <j.u.sophia@qq.com>
2019-06-04 17:06:18 +0800

    'wow'

--- hello.ts
+++
@@ -1,9 +0,0 @@
-function reverse(x: number): number;
-function reverse(x: string): string;
-function reverse(x: number | string): number | string{
-    if (typeof x === 'number') {
-        return Number(x.toString().split('').reverse().join(''))
-    } else {
-        return x.split('').reverse().join('')
-    }
-}
