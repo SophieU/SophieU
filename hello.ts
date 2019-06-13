bab53eb1fe467366c4aaa60e715da11ad07f087d
SophieU <j.u.sophia@qq.com>
2019-06-13 17:01:27 +0800

    'wow'

--- hello.ts
+++
@@ -1,7 +0,0 @@
-function createArray<T = string>(length: number, value: T): Array<T>{
-    let res: T[] = [];
-    for (let i = 0; i < length; i++){
-        res[i] = value;
-    }
-    return res;
-}
