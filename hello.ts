e78179408236eb05e2c9195b79a25155e7d7835c
SophieU <j.u.sophia@qq.com>
2019-06-17 15:26:43 +0800

    wow1

--- hello.ts
+++ hello.ts
@@ -1,18 +1,1 @@
-class Animal{
-    constructor(name) {
-        return this.name;
-    }
-    sayHi() {
-        console.log(`hello,this is ${this.name} talking`)
-    }
-}
-// 继承
-class Cat extends Animal{
-    constructor(name) {
-        super(name); 
-        console.log(this.name);
-    }
-    sayHi() {
-        return `Meow, ` + super.sayHi();
-    }
-}
+class 
