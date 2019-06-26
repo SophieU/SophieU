746978dad7e7806b995f4eff9f55e444bf854aee
SophieU <j.u.sophia@qq.com>
2019-06-26 16:58:17 +0800

    'wow'

--- hello.ts
+++
@@ -1,9 +0,0 @@
-interface CreateArrayFunc {
-    <T>(length: number, value: T): Array<T>;
-}
-let createArray: CreateArrayFunc;
-createArray = function <T>(length: number, value: T): Array<T> { 
-    let res: T[] = [];
-    
-}
-createArray(3,'x')
