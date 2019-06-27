cc9b82bffd97a7454736ffa0cf9b1761995f2dcc
SophieU <j.u.sophia@qq.com>
2019-06-27 16:56:56 +0800

    'wow'

---
+++ hello.ts
@@ -0,0 +1,8 @@
+interface CreateArrayFunc {
+    <T>(length: number, value: T): Array<T>;
+}
+let createArray: CreateArrayFunc;
+createArray = function <T>(length: number, value: T): Array<T> { 
+    let res: T[] = [];
+    
+}
