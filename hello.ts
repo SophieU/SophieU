93f943b042640166120a03231b259dba4bfdd665
SophieU <j.u.sophia@qq.com>
2019-07-12 16:50:58 +0800

    wow8

--- hello.ts
+++ hello.ts
@@ -1,8 +1,8 @@
-function createArray(length: number, value: any): Array<any>{
-    let res = [];
-    for (let i = 0; i < length; i++){
+function createArray<T>(length: number, value: <T>): Array < T > {
+    let res =[];
+    for(let i = 0; i<length; i++) {
         res[i] = value;
     }
     return res;
-}
-createArray(3, 'x')
+    
+} 
