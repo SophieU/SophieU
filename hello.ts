d7f5f06ca7a23c82c6f80ae210aa6741efaf2e28
SophieU <j.u.sophia@qq.com>
2019-07-23 16:39:03 +0800

    wow8

--- hello.ts
+++ hello.ts
@@ -1,3 +1,7 @@
 interface SearchFunc {
     (source: string, subString: string): boolean;
 }
+let mySearch: SearchFunc;
+mySearch = function (source: string, subString: string) {
+    return source.search(subString) !== -1;
+}
