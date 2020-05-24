496eff40332ae787a50bfe943a6ee5c58edfa771
SophieU <j.u.sophia@qq.com>
2020-05-24 10:47:04 +0800

    717

--- src/create.js
+++ src/create.js
@@ -67,7 +67,7 @@ inquirer
             
             fs.writeFile(`${projectName}/package.json`,JSON.stringify(result), 'utf-8', err=>{
                 if(err) console.log(chalk.red(err))
-                console.log('   redis  nginx apollo')
+                console.log('     nginx apollo')
             })
         })
     })
