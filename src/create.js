10309a6441f8e9ca8d1425950d09ff63caa01f39
SophieU <j.u.sophia@qq.com>
2020-05-31 10:48:03 +0800

    531

--- src/create.js
+++ src/create.js
@@ -67,6 +67,7 @@ inquirer
             
             fs.writeFile(`${projectName}/package.json`,JSON.stringify(result), 'utf-8', err=>{
                 if(err) console.log(chalk.red(err))
+                console.log(chalk.green('渲染成功'))
             })
         })
     })
