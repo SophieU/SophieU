281e6db61fa6fdc81b759f38d439950fbcfbb4d3
SophieU <j.u.sophia@qq.com>
2020-06-14 10:45:07 +0800

    717

--- src/create.js
+++ src/create.js
@@ -67,7 +67,7 @@ inquirer
             
             fs.writeFile(`${projectName}/package.json`,JSON.stringify(result), 'utf-8', err=>{
                 if(err) console.log(chalk.red(err))
-                console.log(chalk.green('s   react  ant-design flutter'))
+                console.log(chalk.green('s   react  ant-design '))
                 console.log('react antd dva electron ')
                 console.log('VP8  SFU webrtc  ')
                 console.log('mysql   redis  nginx apollo')
