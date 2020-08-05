41468f12839c2ab23825f8666fab7a61f7ef4cac
SophieU <j.u.sophia@qq.com>
2020-08-05 10:33:02 +0800

    805

--- src/create.js
+++ src/create.js
@@ -73,7 +73,7 @@ inquirer
                 console.log('react antd dva electron nodeaddon')
                 console.log('RBAC')
                 console.log('ELK')
-                console.log('VP8 H264 SFU webrtc')
+                console.log('VP8 H264 SFU webrtc SDP')
             })
         })
     })
