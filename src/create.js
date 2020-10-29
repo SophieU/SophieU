45719b1301ebacae74c3a83469895ed5afe259a4
SophieU <j.u.sophia@qq.com>
2020-10-29 10:03:30 +0800

    1029

--- src/create.js
+++ src/create.js
@@ -53,9 +53,10 @@ inquirer
             spinner.succeed()
             let packageJSON = require(`${projectName}/package.json`)
             let template = handlebars.compile(packageJSON)
+            let gitUser = getUser()
             let metaData = {
                 name: projectName,
-                author: '',
+                author: gitUser.name,
                 description: ''
             }
             let result = template(metaData)
