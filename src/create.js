d0fe41f4cc3dd54bfc31b641a4da177ebd4c779b
SophieU <j.u.sophia@qq.com>
2020-11-21 17:39:01 +0800

    1121

--- src/create.js
+++ src/create.js
@@ -51,7 +51,7 @@ inquirer
             }
             spinner.succeed()
             let content = ''
-            handlebars.compile(content)
+            let template = handlebars.compile(content)
             console.log(chalk.green('项目下载成功'))
             console.log(chalk.grey(' to Get Start: '))        
             console.log(chalk.grey(` cd ${projectName}`))
