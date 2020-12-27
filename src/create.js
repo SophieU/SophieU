2b72816328dfbcace3e3b345e720667256380b50
SophieU <j.u.sophia@qq.com>
2020-12-27 16:47:21 +0800

    1227

--- src/create.js
+++ src/create.js
@@ -5,20 +5,17 @@ const chalk = require('chalk')
 
 const spinner = ora('项目模板下载中...')
 
-const create = function(templateName, projectName){
-    const tempUrl = template[templateName]
-    console.log(tempUrl,projectName)
-    spinner.start()
-    download(`direct:${tempUrl}`,projectName,{clone:true}, err=>{
-        if(err) {
-            spinner.fail()
-            console.log(chalk.red(`项目生成失败：${err}`))
-            return
-        }
-        spinner.succeed()
-        console.log(chalk.green('项目下载成功'))
-        console.log(chalk.grey(' to Get Start: '))        
-        console.log(chalk.grey(` cd ${projectName}`))
-    })
-}
+spinner.start()
+download(`direct:${tempUrl}`,projectName,{clone:true}, err=>{
+    if(err) {
+        spinner.fail()
+        console.log(chalk.red(`项目生成失败：${err}`))
+        return
+    }
+    spinner.succeed()
+    console.log(chalk.green('项目下载成功'))
+    console.log(chalk.grey(' to Get Start: '))        
+    console.log(chalk.grey(` cd ${projectName}`))
+})
+
 module.exports=create
