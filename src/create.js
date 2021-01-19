6db8ad8ddca088e139a24360ff021c6813712e85
SophieU <j.u.sophia@qq.com>
2021-01-19 15:51:15 +0800

    119

--- src/create.js
+++ src/create.js
@@ -9,9 +9,7 @@ const create = function(templateName, projectName){
     const tempUrl = template[templateName]
     spinner.start()
     download(`direct:${tempUrl}`,projectName,{clone:true}, err=>{
-        if(err){
-            console.log(err)
-        }
+        if(err) console.log(err)
     })
 }
 module.exports=create
