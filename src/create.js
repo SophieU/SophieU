a5811e84b476846c8876ac82f83274d75b626a87
SophieU <j.u.sophia@qq.com>
2021-01-21 15:50:33 +0800

    121

--- src/create.js
+++ src/create.js
@@ -2,12 +2,13 @@ const download = require('download-git-repo')
 const template = require('../template.json')
 const ora = require('ora')
 const chalk = require('chalk')
-const program = require('commander')
+
+const spinner = ora('项目模板下载中...')
 
 const create = function(templateName, projectName){
     const tempUrl = template[templateName]
+    spinner.start()
     download(`direct:${tempUrl}`,`${projectName}`,{clone:true}, err=>{
-        console.log('回调函数')
         if(err){
             console.log(err)
         }
