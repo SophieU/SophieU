58565bfeaf592d0f2d0252c1122eb29e20c5f6d1
SophieU <j.u.sophia@qq.com>
2021-01-28 15:43:26 +0800

    128

--- bin/my-cli.js
+++ bin/my-cli.js
@@ -10,7 +10,8 @@ program.version(config.version, "-V, --version")
 
 // 定义使用方法
 program
-    .command("create <template-name> [project-name]")
+    .command("create")
+    .usage("<template-name> [project-name]")
     .description("创建一个模板项目")
     .action( params =>{
         console.log(params)
