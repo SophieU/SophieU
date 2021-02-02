7dc473973bfecd9e21fd3414a39203c7d5bc0ea3
SophieU <j.u.sophia@qq.com>
2021-02-02 15:37:43 +0800

    202

--- src/delete.js
+++ src/delete.js
@@ -28,4 +28,7 @@ inquirer
         delete tplObj[name]
         // 更新template.json
         console.log(chalk.red('模板删除成功，当前'))
+        fs.writeFile(`${__dirname}/../template.json`, JSON.stringify(tplObj), 'utf-8', err=>{
+            if(err) console.log(err)
+        })
     })
