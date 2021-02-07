5ea57a2dbc0f8ce0006403d8f108e3b742dd9a7f
SophieU <j.u.sophia@qq.com>
2021-02-07 15:34:10 +0800

    207

--- src/delete.js
+++ src/delete.js
@@ -1,1 +1,18 @@
 /* 删除模板 */
+
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const fs = require('fs')
+const tplObj = require('../template.json')
+
+let question = [
+    {
+        name: 'name',
+        message: '请输入要删除的模板名称',
+        validate(name){
+            if(name===''){
+                return 'Name is required'
+            }
+        }
+    }
+]
