ebafdc625ea828013d8c32b416de771f8ad7745e
SophieU <j.u.sophia@qq.com>
2021-02-28 14:46:35 +0800

    local

--- src/create.js
+++ src/create.js
@@ -3,3 +3,9 @@ const template = require('../template.json')
 
 const tempUrl = template.admin.url
 
+download(`direct:${tempUrl}`,'./tmp',{clone:true}, err=>{
+    console.log('回调函数')
+    if(err){
+        console.log(err)
+    }
+})
