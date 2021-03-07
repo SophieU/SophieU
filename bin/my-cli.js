c76dfe203218e3c4461d5c27212b3b0f17dbdb3f
SophieU <j.u.sophia@qq.com>
2021-03-07 14:32:55 +0800

    add

--- bin/my-cli.js
+++ bin/my-cli.js
@@ -12,6 +12,14 @@ program
         require("../src/create")
     })
 
+program
+    .command("list")
+    .description("列出当前可用模板项目")
+    .action(()=>{
+        require('../src/list')
+    })
+
+// 解析参数
 program.parse(process.argv)
 if(!program.args.length){
     program.help()
