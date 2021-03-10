11d429bfd7c9a33321671f607ea007800ef10415
SophieU <j.u.sophia@qq.com>
2021-03-10 14:31:28 +0800

    load

--- bin/my-cli.js
+++ bin/my-cli.js
@@ -2,5 +2,18 @@
 const program = require('commander')
 const config = require('../package.json')
 
-program.version(config.version)
+program.version(config.version, "-V, --version")
+
+// 定义使用方法
+program
+    .command("create <template-name> [project-name]")
+    .description("创建一个模板项目")
+    .action(()=>{
+        require("../src/create")
+    })
+
+program.parse(process.argv)
+if(!program.args.length){
+    program.help()
+}
 console.log(process.argv)
